a6923e45bd80cf166d99868cc452a2a02823d39c
Jabari Long <mrjabarilong@gmail.com>
2024-07-15 21:46:13 -0400

    rmv jinja line

--- create_tble.sql
+++ create_tble.sql
@@ -1,10 +1,10 @@
 -- Create file "create_customer_table.sql" and commit it to your git repo
 -- The file should have the following contents:
-#!jinja
+
 CREATE TABLE git_demo_customer (
   id number not null, 
   first_name varchar, 
   last_name varchar
 );
-GRANT OWNERSHIP ON TABLE git_demo_customer TO ROLE {{owner}};
+GRANT OWNERSHIP ON TABLE git_demo_customer TO ROLE SYSADMIN;
 EXECUTE IMMEDIATE FROM 'insert_records.sql';
