118ea1d7aadc06a24aeb9f7fd1b6ded35de72fd2
Jabari Long <mrjabarilong@gmail.com>
2024-07-16 17:47:22 -0400

    add new test files

---
+++ cust.sql
@@ -0,0 +1,10 @@
+-- Create file "create_customer_table.sql" and commit it to your git repo
+-- The file should have the following contents:
+--!jinja
+CREATE TABLE customers1 (
+  id number not null, 
+  first_name varchar, 
+  last_name varchar
+);
+GRANT OWNERSHIP ON TABLE customer TO ROLE {{owner}};
+EXECUTE IMMEDIATE FROM 'insert_records1.sql';
